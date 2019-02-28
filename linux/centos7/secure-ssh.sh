1232cd40506dd95813367c1c4673519152c6c359
ryan <ryan.heniges@mymail.champlain.edu>
2019-02-27 21:21:54 -0500

    Updated secure-ssh.sh

--- linux/centos7/secure-ssh.sh
+++ linux/centos7/secure-ssh.sh
@@ -4,4 +4,9 @@
 #Adds a public key to that users authorized keys file
 #Removes roots ability to ssh in
 
-echo "testing 123"
+useradd sys265
+mkdir /home/sys265/.ssh
+cp linux/public-keys/sys265.pub /home/sys265/.ssh/authorized_keys
+chmod 700 /home/sys265/.ssh
+chmod 600 /home/sys265/.ssh/authorized_keys
+chown -R sys265:sys265 /home/sys265/.ssh
